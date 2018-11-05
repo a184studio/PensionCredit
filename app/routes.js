9267d9719a987ca65e49aa98c83c285cf2fc1af5
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2018-11-05 10:15:00 +0000

    Add missing routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.use(require('./routes/new-claims'));
 
 module.exports = router
