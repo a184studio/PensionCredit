acee7f802593116cba50275817ab712a2e092d32
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2018-11-05 11:46:10 +0000

    Add more of the new claims journey

--- app/routes/new-claims.js
+++ app/routes/new-claims.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = new express.Router();
 
-router.post('/new-claims/has-partner-router', function (req, res) {
+router.post('/new-claims/has-partner-router', (req, res) => {
   const hasPartner = req.session.data['has-partner']
 
   if (hasPartner === 'yes') {
@@ -12,7 +12,7 @@ router.post('/new-claims/has-partner-router', function (req, res) {
   }
 })
 
-router.post('/new-claims/care-home-router', function (req, res) {
+router.post('/new-claims/care-home-router', (req, res) => {
   const inCareHome = req.session.data['care-home']
 
   if (inCareHome === 'yes') {
@@ -22,7 +22,7 @@ router.post('/new-claims/care-home-router', function (req, res) {
   }
 })
 
-router.post('/new-claims/care-home-permanence-router', function (req, res) {
+router.post('/new-claims/care-home-permanence-router', (req, res) => {
   const permanence = req.session.data['care-home-permanence']
 
   if (permanence === 'yes') {
@@ -32,17 +32,17 @@ router.post('/new-claims/care-home-permanence-router', function (req, res) {
   }
 })
 
-router.post('/new-claims/address-choice-router', function (req, res) {
+router.post('/new-claims/address-choice-router', (req, res) => {
   const inCareHome = req.session.data['care-home']
 
   if (inCareHome === 'no') {
     res.redirect('/new-claims/home-ownership')
   } else {
-    res.redirect('/new-claims/bbc')
+    res.redirect('/new-claims/money-you-have')
   }
 })
 
-router.post('/new-claims/home-ownership-router', function (req, res) {
+router.post('/new-claims/home-ownership-router', (req, res) => {
   const homeOwnership = req.session.data['home-ownership']
 
   if (homeOwnership === 'own') {
@@ -54,13 +54,63 @@ router.post('/new-claims/home-ownership-router', function (req, res) {
   }
 })
 
-router.post('/new-claims/money-you-have-router', function (req, res) {
+router.post('/new-claims/money-you-have-router', (req, res) => {
   const hasMoney = req.session.data['has-money']
 
   if (hasMoney === 'yes') {
     res.redirect('/new-claims/cash')
   } else {
-    res.redirect('/new-claims/earnings')
+    res.redirect('/new-claims/has-private-pension')
+  }
+})
+
+router.post('/new-claims/has-private-pension-router', (req, res) => {
+  const hasPensions = req.session.data['has-private-pensions']
+
+  if (hasPensions === 'yes') {
+    res.redirect('/new-claims/about-your-pensions')
+  } else {
+    res.redirect('/new-claims/has-work')
+  }
+})
+
+router.post('/new-claims/has-work-router', (req, res) => {
+  const hasWork = req.session.data['has-work']
+
+  if (hasWork === 'yes') {
+    res.redirect('/new-claims/about-your-employment')
+  } else {
+    res.redirect('/new-claims/self-employed')
+  }
+})
+
+router.post('/new-claims/self-employed-router', (req, res) => {
+  const selfEmployed = req.session.data['self-employed']
+
+  if (selfEmployed === 'yes') {
+    res.redirect('/new-claims/about-your-self-employment')
+  } else {
+    res.redirect('/new-claims/other-income')
+  }
+})
+
+router.post('/new-claims/other-income-router', (req, res) => {
+  const otherIncome = req.session.data['other-income']
+
+  if (otherIncome === 'yes') {
+    res.redirect('/new-claims/about-your-other-income')
+  } else {
+    res.redirect('/new-claims/type-of-account')
+  }
+})
+
+router.post('/new-claims/type-of-account-router', (req, res) => {
+  const accountType = req.session.data['account-type']
+
+  if (accountType === 'uk') {
+    res.redirect('/new-claims/uk-account')
+  } else {
+    res.redirect('/new-claims/international-account')
   }
 })
 
