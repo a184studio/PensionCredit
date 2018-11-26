c0c65cfa232fa729e7d03dd775b4317f50ccc501
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2018-11-26 15:32:18 +0000

    Big iterative change on short-claim

--- app/routes/sprint-2/short-claim.js
+++ app/routes/sprint-2/short-claim.js
@@ -3,45 +3,56 @@ const express = require('express')
 const router = new express.Router()
 const baseUrl = '/sprint-2/short-claim'
 
-router.post(`${baseUrl}/has-partner-router`, (req, res) => {
-  const hasPartner = req.session.data['has-partner']
-
-  if (hasPartner === 'yes') {
-    res.redirect(`${baseUrl}/about-partner`)
-  } else {
-    res.redirect(`${baseUrl}/self-employed`)
-  }
-})
-
-router.post(`${baseUrl}/self-employed-router`, (req, res) => {
+router.post(`${baseUrl}/about-you-router`, (req, res) => {
   const selfEmployed = req.session.data['self-employed']
+  const rightToAbode = req.session.data['right-to-abode']
 
-  if (selfEmployed === 'yes') {
+  if (rightToAbode === 'no') {
+    res.redirect(`${baseUrl}/not-eligible`)
+  } else if (selfEmployed === 'yes') {
     res.redirect(`${baseUrl}/notify-cas`)
   } else {
-    res.redirect(`${baseUrl}/right-to-abode`)
+    res.redirect(`${baseUrl}/postcode-lookup`)
   }
 })
 
 router.post(`${baseUrl}/home-ownership-router`, (req, res) => {
   const ownhome = req.session.data['home-ownership']
 
-  if (ownhome === 'own') {
+  if (ownhome === 'Own') {
     res.redirect(`${baseUrl}/ground-rent`)
-  } else if (ownhome === 'rent') {
-    res.redirect(`${baseUrl}/has-partner`)
   } else {
     res.redirect(`${baseUrl}/has-partner`)
   }
 })
 
-router.post(`${baseUrl}/right-to-abode-router`, (req, res) => {
-  const rightToAbode = req.session.data['right-to-abode']
+router.post(`${baseUrl}/has-partner-router`, (req, res) => {
+  const hasPartner = req.session.data['has-partner']
 
-  if (rightToAbode === 'yes') {
-    res.redirect(`${baseUrl}/money-you-have`)
+  if (hasPartner === 'yes') {
+    res.redirect(`${baseUrl}/about-partner`)
   } else {
-    res.redirect(`${baseUrl}/not-eligible`)
+    res.redirect(`${baseUrl}/has-private-pensions`)
+  }
+})
+
+router.post(`${baseUrl}/about-partner-router`, (req, res) => {
+  const selfEmployed = req.session.data['partner-self-employed']
+
+  if (selfEmployed === 'yes') {
+    res.redirect(`${baseUrl}/notify-cas`)
+  } else {
+    res.redirect(`${baseUrl}/has-private-pensions`)
+  }
+})
+
+router.post(`${baseUrl}/has-private-pensions-router`, (req, res) => {
+  const hasPrivatePensions = req.session.data['has-private-pensions']
+
+  if (hasPrivatePensions === 'yes') {
+    res.redirect(`${baseUrl}/about-pension`)
+  } else {
+    res.redirect(`${baseUrl}/money-you-have`)
   }
 })
 
@@ -52,16 +63,6 @@ router.post(`${baseUrl}/money-you-have-router`, (req, res) => {
     res.redirect(`${baseUrl}/capital`)
   } else {
     res.redirect(`${baseUrl}/second-property`)
-  }
-})
-
-router.post(`${baseUrl}/second-property-router`, (req, res) => {
-  const hasSecondProperty = req.session.data['second-property']
-
-  if (hasSecondProperty === 'yes') {
-    res.redirect(`${baseUrl}/not-eligible`)
-  } else {
-    res.redirect(`${baseUrl}/check-your-answers`)
   }
 })
 
