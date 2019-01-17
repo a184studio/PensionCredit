a83c49bdfbb5dd2c99c685690fca4af1836edaa9
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2019-01-17 10:48:33 +0000

    Add MVP routes to routes.js file

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,6 @@ router.use(require('./routes/sprint-2/short-claim'))
 router.use(require('./routes/sprint-2/monolith'))
 router.use(require('./routes/sprint-3/new-claims'))
 router.use(require('./routes/sprint-4/new-claims'))
+router.use(require('./routes/sprint-4/mvp'))
 
 module.exports = router
