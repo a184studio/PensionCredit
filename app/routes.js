1498aa0d502ae20bd22019c60e2fdbfb4bd8ec1d
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2019-02-28 14:30:32 +0000

    Add new routes to routes.js file

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,8 @@ router.use(require('./routes/sprint-2/monolith'))
 router.use(require('./routes/sprint-3/new-claims'))
 router.use(require('./routes/sprint-4/new-claims'))
 router.use(require('./routes/sprint-4/mvp'))
+router.use(require('./routes/sprint-5/new-claims'))
+router.use(require('./routes/sprint-5/mvp'))
 router.use(require('./routes/declaration-test/dec'))
 router.use(require('./routes/pdf'))
 
