831402ca58cf87c264763946532e7eda5f3d414e
contentpete <44030075+contentpete@users.noreply.github.com>
2019-03-26 15:59:50 +0000

    Content changes

--- app/routes/development/wf.js
+++ app/routes/development/wf.js
@@ -1,12 +1,4 @@
 const express = require('express')
-const { getStatePensionDate } = require('get-uk-state-pension-date')
-const differenceInDays = require('date-fns/difference_in_days')
-const startOfDay = require('date-fns/start_of_day')
-const subMonths = require('date-fns/sub_months')
-const got = require('got')
-const fs = require('fs')
-const {getMonth} = require('../../filters')()
-
 const router = new express.Router()
 const baseUrl = '/development/winter-fuel'
 
