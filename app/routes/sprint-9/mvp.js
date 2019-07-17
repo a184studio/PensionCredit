90282b19105331fcecc3cc01c2d74b54c57a0cc0
Mark McCormick <markmccormick@A184.local>
2019-07-17 17:59:20 +0100

    MM: MSIC summary updated content 17 July 2019

--- app/routes/sprint-9/mvp.js
+++ app/routes/sprint-9/mvp.js
@@ -621,6 +621,21 @@ router.post(`${baseUrl}/msic-has-post-office-card-account-yn-router`, (req, res)
 })
 
 
+
+router.post(`${baseUrl}/msic-has-credit-union-yn-router`, (req, res) => {
+  const hasCreditUnionAccount = req.session.data['msic-has-credit-union-account-yn']
+
+  if (hasCreditUnionAccount === 'yes') {
+    res.redirect(`${baseUrl}/msic-credit-union-account`)
+  } else {
+    res.redirect(`${baseUrl}/msic-has-premium-bond-account-yn`)
+  }
+})
+
+
+
+
+
 router.post(`${baseUrl}/msic-has-premium-bond-account-yn-router`, (req, res) => {
   const hasPremiumBondAccount = req.session.data['msic-has-premium-bond-account-yn']
 
