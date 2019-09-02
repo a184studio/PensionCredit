0861e0abbe026ede3a109355b2e0f8253904b80d
Mark McCormick <mark@a184.uk>
2019-09-02 11:37:34 +0100

    MM: TaskList and Single jounrey for A.2.5

--- app/routes/sprint-9/sept-3-2019.js
+++ app/routes/sprint-9/sept-3-2019.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const {getMonth} = require('../../filters')()
 
 const router = new express.Router()
-const baseUrl = '/sprint-9/mvp'
+const baseUrl = '/sprint-9/sept-3-2019'
 
 function makeAStay(data) {
   const admission = new Date(`${data['admission-year']}-${data['admission-month']}-${data['admission-day']}`)
