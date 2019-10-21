339ed530f77ae1a580265a63fbf8b391fc9deca9
Mark McCormick <mark@a184.uk>
2019-10-21 09:20:38 +0100

    MM: A&B tests

--- app/routes/sprint-10/pc-payment-b.js
+++ app/routes/sprint-10/pc-payment-b.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const {getMonth} = require('../../filters')()
 
 const router = new express.Router()
-const baseUrl = '/sprint-10/pc-payment-a'
+const baseUrl = '/sprint-10/pc-payment-b'
 
 function makeAStay(data) {
   const admission = new Date(`${data['admission-year']}-${data['admission-month']}-${data['admission-day']}`)
