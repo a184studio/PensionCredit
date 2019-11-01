d4d59c0366d91c3e9382503e7a55dd3306c89ffa
contentpete <peter.bates@engineering.digital.dwp.gov.uk>
2019-11-01 11:25:39 +0000

    Backdating routing

--- app/routes/sprint-10/backdating.js
+++ app/routes/sprint-10/backdating.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const {getMonth} = require('../../filters')()
 
 const router = new express.Router()
-const baseUrl = '/sprint-10/mvp'
+const baseUrl = '/sprint-10/backdating'
 
 function makeAStay(data) {
   const admission = new Date(`${data['admission-year']}-${data['admission-month']}-${data['admission-day']}`)
