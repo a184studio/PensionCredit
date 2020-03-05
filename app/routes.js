6188f640689bc325b1bc1072537c7275bacdaf80
Mark McCormick <mark@a184.uk>
2020-03-05 15:51:19 +0000

    MM: State pension details pages

--- app/routes.js
+++ app/routes.js
@@ -39,9 +39,7 @@ router.use(require('./routes/version-13/mvp'))
 router.use(require('./routes/version-13/gci-pensions'))
 router.use(require('./routes/version-13/cis-benefits'))
 router.use(require('./routes/version-13/context'))
-router.use(require('./routes/version-14/mvp'))
-router.use(require('./routes/version-14/gci-pensions'))
-router.use(require('./routes/version-14/cis-benefits'))
+router.use(require('./routes/version-14/APIs'))
 
 
 module.exports = router
