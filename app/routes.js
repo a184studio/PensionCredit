bfe4f1cc0985a01e912e19bdc53959d3dce75e53
Mark McCormick <mark@a184.uk>
2020-04-06 17:33:04 +0100

    MM: CF MVP

--- app/routes.js
+++ app/routes.js
@@ -43,6 +43,8 @@ router.use(require('./routes/version-14/APIs'))
 router.use(require('./routes/version-15/APIs'))
 router.use(require('./routes/version-15/survey'))
 router.use(require('./routes/version-15/index'))
+router.use(require('./routes/citizen-version-1/mvp'))
+
 
 
 module.exports = router
