9339eb20cceab733e52b28b988b9dcb2e4f00239
Mark McCormick <mark@a184.uk>
2020-04-17 15:56:18 +0100

    MM: Fixes

--- app/routes.js
+++ app/routes.js
@@ -45,7 +45,7 @@ router.use(require('./routes/version-15/survey'))
 router.use(require('./routes/version-15/index'))
 router.use(require('./routes/version-15/index-2'))
 router.use(require('./routes/citizen-version-1/mvp'))
-router.use(require('./routes/citizen-version-1/work-list'))
+router.use(require('./routes/citizen-version-1/work-list-1-2'))
 router.use(require('./routes/citizen-version-1-1/mvp'))
 
 
