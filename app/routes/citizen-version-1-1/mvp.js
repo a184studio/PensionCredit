7e9b6c30f6594519a391422ce2850cecbf787c80
Mark McCormick <mark@a184.uk>
2020-04-23 12:32:26 +0100

    MM: Fix

--- app/routes/citizen-version-1-1/mvp.js
+++ app/routes/citizen-version-1-1/mvp.js
@@ -868,7 +868,7 @@ router.post(`${baseUrl}/money-second-property-router`, (req, res) => {
   if (hasSecondProperty === 'Yes') {
     res.redirect(`${baseUrl}/money-disregards-all`)
   } else {
-    res.redirect(`${baseUrl}/final-CYA`)
+    res.redirect(`${baseUrl}/final-cya`)
   }
 })
 
