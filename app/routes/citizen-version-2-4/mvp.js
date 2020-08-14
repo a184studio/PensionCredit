28a5bcc5c44465dea53f0cdc9c65181714d2b978
Mark McCormick <mark@a184.uk>
2020-08-14 14:41:40 +0100

    MM: Bonds router update

--- app/routes/citizen-version-2-4/mvp.js
+++ app/routes/citizen-version-2-4/mvp.js
@@ -805,6 +805,18 @@ router.post(`${baseUrl}/disregards-types-router`, (req, res) => {
 
 
 
+router.post(`${baseUrl}/money-second-property-router`, (req, res) => {
+  const moneySecondProperty = req.session.data['money-second-property-check']
+
+  if (moneySecondProperty === 'Yes') {
+    res.redirect(`${baseUrl}/money-bonds`)
+  } else {
+    res.redirect(`${baseUrl}/hrt-claimant-immigration-restrictions`)
+  }
+})
+
+
+
 // disregards END
 
 
