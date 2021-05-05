e3eed022d35a2c3e8b8b6fc084db88b717347b33
A184 <mark@a184.uk>
2021-05-05 10:46:14 +0100

    MM: V2.10

--- app/routes.js
+++ app/routes.js
@@ -84,6 +84,7 @@ router.use(require('./routes/idv-0-1/mvp'))
 router.use(require('./routes/g4s-0-4/mvp'))
 router.use(require('./routes/agent-worklist-2-4/worklist'))
 router.use(require('./routes/citizen-version-2-9-ur/mvp'))
+router.use(require('./routes/citizen-version-2-10/mvp'))
 
 
 module.exports = router
