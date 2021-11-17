aa02fd7477ba333a4bf52b73bbff61beecc5d425
A184 <mark@a184.uk>
2021-11-17 12:14:16 +0000

    MM: PDF

--- app/routes/citizen-version-2-12/mvp.js
+++ app/routes/citizen-version-2-12/mvp.js
@@ -23,6 +23,7 @@ function makeAStay(data) {
 // PDF DOWNLOADER
 router.use(`${baseUrl}/claim.pdf`, express.static(path.resolve('app/views/citizen-version-2-8-ur/mvp/claim.pdf'))) // ../ back up a directory
 router.use(`${baseUrl}/abc.pdf`, express.static(path.resolve('app/views/citizen-version-2-12/mvp/abc.pdf'))) // ../ back up a directory
+router.use(`${baseUrl}/abc.pdf`, express.static(path.resolve('app/views/citizen-version-2-12/mvp/letter-17-nov.pdf'))) // ../ back up a directory
 
 // —————————————————————————————————
 
